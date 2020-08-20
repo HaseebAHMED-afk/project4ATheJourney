250d0905eae1b07fbe266f57057e71fc76b0f46c
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-08-20 19:29:36 +0500

    Done with artwork

---
+++ index.js
@@ -0,0 +1,12 @@
+var background = document.querySelector(".background");
+
+var frames = [
+    
+]
+
+var timing = {
+    duration : 10000,
+    iterations : Infinity
+}
+
+background.animate(frames , timing);
